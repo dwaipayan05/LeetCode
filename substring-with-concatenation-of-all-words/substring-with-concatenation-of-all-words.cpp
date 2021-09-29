59b15ad4fcb1e8bc0a3ea44dece4bfa544a7018e
Dwaipayan Munshi <53687927+dwaipayan05@users.noreply.github.com>
2021-09-29 09:44:09 +0530

    Time: 268 ms (50.05%), Space: 22.5 MB (78.84%) - LeetHub

--- substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
+++ substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -2,45 +2,50 @@ class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
         
-        if(s.length() == 0 || words.size() == 0 )
+        if(s.length() == 0 || words.size() == 0)
             return {};
         
-        int n = words[0].size();
-        int len = words.size() * words[0].size();
+        map<string,int>mp;
         
-        if(len > s.length())
-            return {};
-        map<string, int>mp;
         for(auto s : words)
             mp[s]++;
         
         vector<int>res;
         
-        for(int i = 0; i <= s.length() - len; i++ )
+        int concatLen = words.size()*words[0].length();
+        int wordLen = words[0].length();
+        int wordCnt = words.size();
+        
+        if(concatLen > s.length())
+            return {};
+        
+        
+        for(int i = 0; i <= s.length() - concatLen ; i++)
         {
-            map<string, int> Seen;
+            map<string,int>wordsSeen;
             
-            for(int j = 0 ; j < words.size(); j++)
+            for(int j = 0; j < wordCnt; j++)
             {
-                int wordIndex = i + j*words[0].length();
-                string curr_word = s.substr(wordIndex, words[0].length());
-                if(mp.find(curr_word) == mp.end())
-                    break;            
-                Seen[curr_word]++;
+                int wordIdx = i + j*wordLen;
+                string currWord = s.substr(wordIdx, wordLen);
                 
-                if(Seen[curr_word] > mp[curr_word])
+                if(mp.find(currWord) == mp.end())
+                    break;
+                
+                wordsSeen[currWord]++;
+                
+                if(wordsSeen[currWord] > mp[currWord])
                     break;
                 
                 
-                if(j + 1 == words.size())
+                if(j+1 == wordCnt)
                     res.push_back(i);
                 
-            }
+            }           
+            
         }
         
         
         return res;
-        
-        
     }
 };
