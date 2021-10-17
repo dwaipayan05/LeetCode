e540a5107153b52b7b38a15daa26d69f63f321e7
Dwaipayan Munshi <53687927+dwaipayan05@users.noreply.github.com>
2021-10-17 20:04:35 +0530

    Time: 20 ms (66.65%), Space: 15.7 MB (65.27%) - LeetHub

---
+++ path-sum-iii/path-sum-iii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int ans=0;
+    int pathSum(TreeNode* root, int sum) {
+        if(root){
+            dfs(root,sum);
+            pathSum(root->left,sum);
+            pathSum(root->right,sum);
+        }
+        return ans;
+    }
+    void dfs(TreeNode* root, int sum){
+        if(!root)return;
+        if(root->val==sum)ans++;
+        dfs(root->left,sum-root->val);
+        dfs(root->right,sum-root->val);
+    }
+};
